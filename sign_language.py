c09ec80f0642e03b67738af2b480082c2319053d
SakshiAg02 <113249274+SakshiAg02@users.noreply.github.com>
2023-03-05 13:41:32 +0530

    Add files via upload

--- sign_language.py
+++ sign_language.py
@@ -23,25 +23,28 @@ while True:
             for id ,lm in enumerate(hand_landmark.landmark):
                 lm_list.append(lm)
                 
-    for tip in finger_tips:
-        x,y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
-        cv2.circle(img,(x,y),15,(255,0,0),cv.FILLED)
-        finger_fold_status = []
-    
-    if lm_list[tip].x < lm_list[tip - 3].x:
-        cv2.circle(img,(x,y),15,(0,0,255),cv2.FILLED)
-        finger_fold_status.append(True)
-    else:
-        finger_fold_status.append(False)
-        
-    if all(finger_fold_status):
-        if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y< lm_list[thumb_tip-2].y:
-            print("LIKE")
-            cv2.putText(img,"LIKE",(20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
+            finger_fold_status = []
             
-        if lm_list[thumb_tip].y > lm_list[thumb_tip-1].y > lm_list[thumb_tip-2].y:
-            print("DISLIKE")
-            cv2.putText(img,"DISLIKE",(20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0),3)
+            for tip in finger_tips:
+                x,y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
+                cv2.circle(img,(x,y),15,(255,0,0),cv2.FILLED)
+                
+    
+                if lm_list[tip].x < lm_list[tip - 3].x:
+                    cv2.circle(img,(x,y),15,(0,0,255),cv2.FILLED)
+                    finger_fold_status.append(True)
+                else:
+                    finger_fold_status.append(False)
+            print(finger_fold_status)
+        
+            if all(finger_fold_status):
+                if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y< lm_list[thumb_tip-2].y:
+                    print("LIKE")
+                    cv2.putText(img,"LIKE",(20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
+            
+                if lm_list[thumb_tip].y > lm_list[thumb_tip-1].y > lm_list[thumb_tip-2].y:
+                    print("DISLIKE")
+                    cv2.putText(img,"DISLIKE",(20,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0),3)
             
             mp_draw.draw_landmarks(img, hand_landmark,
             mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
